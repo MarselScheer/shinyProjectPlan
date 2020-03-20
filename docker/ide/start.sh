7c8c6d1b80c75c5bae0f03809e8966f8e3e11276
m <scheer@freescience.de>
2020-03-20 23:20:23 +0100

    docker path to host-file-system updated

--- docker/ide/start.sh
+++ docker/ide/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo docker run -d -p 8787:8787 -e DISABLE_AUTH=true -v /mnt/samba/tmp/github_repos:/home/rstudio shiny_project_plan_ide
+sudo docker run -d -p 8787:8787 -e DISABLE_AUTH=true -v ~/docker_fs:/home/rstudio shiny_project_plan_ide
